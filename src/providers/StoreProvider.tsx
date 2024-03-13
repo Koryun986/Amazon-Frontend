eae2c2e8a3cfb657f98af31d7cc91c510c589ee6
KoryunWeConstruct <koryun986@gmail.com>
2024-03-13 14:21:50 +0400

    refactor(store provider)

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -3,8 +3,6 @@ import {useRef} from "react";
 import makeStore, {AppStore} from "../redux/store";
 import {Provider} from "react-redux";
 
-
-
 export default function StoreProvider({
     children
 }: {
