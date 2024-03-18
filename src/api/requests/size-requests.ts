1bf3a622cf95a45bfed6ebc51c2d195a92fc9dff
KoryunWeConstruct <koryun986@gmail.com>
2024-03-18 21:08:11 +0400

    feat(size select)

--- src/api/requests/size-requests.ts
+++ src/api/requests/size-requests.ts
@@ -3,17 +3,17 @@ import api from "../index";
 import {ApiConstants} from "../api-constants";
 
 export async function getSizes() {
-    return await api.get<ISize>(ApiConstants.COLORS_GET_ALL);
+    return await api.get<ISize>(ApiConstants.SIZES_GET_ALL);
 }
 
 export async function createSize(name: string) {
-    return await api.post(ApiConstants.COLOR_CREATE, {name})
+    return await api.post(ApiConstants.SIZE_CREATE, {name})
 }
 
 export async function updateSize(size: ISize) {
-    return await api.put(ApiConstants.COLOR_UPDATE, size);
+    return await api.put(ApiConstants.SIZE_UPDATE, size);
 }
 
 export async function deleteSize(id: number) {
-    return await api.delete(`${ApiConstants.COLOR_DELETE}/${id}`)
+    return await api.delete(`${ApiConstants.SIZE_DELETE}/${id}`)
 }
--- src/components/SideBarMenu.tsx
+++ src/components/SideBarMenu.tsx
@@ -1,36 +1,16 @@
 "use client"
-import {Menu, MenuProps, TreeDataNode, TreeSelect} from "antd";
 import {useAppDispatch, useAppSelector} from "../hooks/store-hooks";
 import {useEffect, useState} from "react";
 import {fetchAddresses} from "../redux/slices/user-address-slice";
 import dynamic from "next/dynamic";
 import {fetchCategories} from "../redux/slices/category-slice";
-import {ICategory} from "../types/ICategory";
-import useFilterByParams from "../hooks/filter-by-params-hook";
 import {fetchColors} from "../redux/slices/color-slice";
 import ColorsSelect from "./ColorsSelect";
 import CategorySelect from "./CategorySelect";
+import SizeSelect from "./SizeSelect";
+import {fetchSizes} from "../redux/slices/size-slice";
 
 const AddressMenu = dynamic(() => import("./AddressMenu"));
-
-type MenuItem = Required<MenuProps>['items'][number];
-function getItem(
-    label: React.ReactNode,
-    key?: React.Key | null,
-    icon?: React.ReactNode,
-    children?: MenuItem[] | null,
-    type?: 'group',
-): MenuItem {
-    return {
-        key,
-        icon,
-        children,
-        label,
-        type,
-    } as MenuItem;
-}
-
-const { SHOW_CHILD } = TreeSelect;
 
 const SideBarMenu = () => {
     const dispatch = useAppDispatch();
@@ -42,12 +22,14 @@ const SideBarMenu = () => {
         }
         dispatch(fetchCategories());
         dispatch(fetchColors());
+        dispatch(fetchSizes());
     }, [user]);
 
     return (
         <>
             {!!user && <AddressMenu />}
             <ColorsSelect />
+            <SizeSelect />
             <CategorySelect />
         </>
     );
