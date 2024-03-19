b4bd9f7f9860ecbb5647f55ad4862fe049b15f18
KoryunWeConstruct <koryun986@gmail.com>
2024-03-19 17:35:39 +0400

    refactor: delete unused imports and variables

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,15 +1,9 @@
 "use client"
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import {HeartOutlined, ShoppingOutlined} from "@ant-design/icons";
 import {Flex} from "antd";
-import {useAppSelector} from "../hooks/store-hooks";
-
-const Button = dynamic(() => import("antd").then(antd => antd.Button), {ssr: false});
 
 const HeaderNav = () => {
-    const user = useAppSelector(state => state.user.user);
-
     return (
         <Flex align={"center"} gap={"middle"}>
             <Link href={"/cart-items"} >
