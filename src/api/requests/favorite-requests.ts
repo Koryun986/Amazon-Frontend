073751bb9ee1979b26219978e03a68a728401ce4
KoryunWeConstruct <koryun986@gmail.com>
2024-03-20 16:14:16 +0400

    fix(bug with favorites fetch): provide data after doing fetch

--- src/api/requests/favorite-requests.ts
+++ src/api/requests/favorite-requests.ts
@@ -3,17 +3,17 @@ import {ApiConstants} from "../api-constants";
 import type {IFavorite} from "../../types/IFavorite";
 
 export async function getFavorites() {
-    return await api.get<IFavorite[]>(ApiConstants.FAVORITES_GET_ALL);
+    return(await api.get<IFavorite[]>(ApiConstants.FAVORITES_GET_ALL)).data;
 }
 
 export async function addFavoriteRequest(productId: number) {
-    return await api.post(ApiConstants.FAVORITE_ADD, {product_id: productId});
+    return (await api.post(ApiConstants.FAVORITE_ADD, {product_id: productId})).data;
 }
 
 export async function addManyFavoritesRequest(productIds: number[]) {
-    return await api.post(ApiConstants.FAVORITE_ADD_MANY, productIds);
+    return (await api.post(ApiConstants.FAVORITE_ADD_MANY, productIds)).data;
 }
 
 export async function removeFavoriteRequest(productId: number) {
-    return await api.delete(`${ApiConstants.FAVORITE_REMOVE}/${productId}`)
+    return (await api.delete(`${ApiConstants.FAVORITE_REMOVE}/${productId}`)).data;
 }
