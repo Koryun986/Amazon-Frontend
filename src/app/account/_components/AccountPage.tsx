7d7d0d5eebae0e6a1a9ba10c78e07e7eacdf9d31
KoryunWeConstruct <koryun986@gmail.com>
2024-03-20 21:22:21 +0400

    refactor(account header): create separate component for header

--- src/app/account/_components/AccountPage.tsx
+++ src/app/account/_components/AccountPage.tsx
@@ -1,14 +1,10 @@
 "use client"
 import {useEffect, useState} from "react";
-import Link from "next/link";
-import {Avatar, Button, Card, Divider, Dropdown, Space} from "antd";
-import type {MenuProps} from "antd";
-import {HeartOutlined, HomeOutlined, ShoppingCartOutlined} from "@ant-design/icons";
 import {getUser} from "../../../api/requests/auth-requests";
 import {LocalStorageConstants} from "../../../constants/localstorage-constants";
 import {setUser} from "../../../redux/slices/user-slice";
 import {useAppDispatch, useAppSelector} from "../../../hooks/store-hooks";
-import HeaderMenu from "./HeaderMenu";
+import AccountHeader from "./AccountHeader";
 import type {IUser} from "../../../types/IUser";
 
 export type AccountPageList = "Favorites" | "Cart Items" | "Addresses" | "Products"
@@ -40,13 +36,7 @@ const AccountPage = () => {
     return (
         <div className="container mx-auto">
             {user && (
-                <div className="py-8 flex items-center justify-between">
-                    <Space direction={"horizontal"}>
-                        <Avatar size={40} style={{backgroundColor: "#001529", fontSize: "16px"}}>{user.first_name[0]} {user.last_name[0]}</Avatar>
-                        <div>{user.email}</div>
-                    </Space>
-                    <HeaderMenu current={listName} setCurrent={setListName} />
-                </div>
+                <AccountHeader user={user} menuItem={listName} setMenuItem={setListName} />
             )}
         </div>
     )
