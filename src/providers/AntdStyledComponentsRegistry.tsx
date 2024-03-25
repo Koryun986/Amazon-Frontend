62efa9d15e1223f92e068d9546d6417a799a0cb7
KoryunWeConstruct <koryun986@gmail.com>
2024-03-25 15:07:47 +0400

    chore(antd style cache)

--- src/providers/AntdStyledComponentsRegistry.tsx
+++ src/providers/AntdStyledComponentsRegistry.tsx
@@ -5,11 +5,11 @@ import { useServerInsertedHTML } from 'next/navigation';
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
 
 export default function AntdStyledComponentsRegistry({ children }: { children: React.ReactNode }) {
-  const [cache] = React.useState(() => createCache());
+  const cache = React.useMemo(() => createCache(), []);
 
   useServerInsertedHTML(() => (
     <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}></style>
   ));
 
-  return <StyleProvider cache={cache}>{children}</StyleProvider>;
+  return <StyleProvider cache={cache} hashPriority={"high"}>{children}</StyleProvider>;
 }
