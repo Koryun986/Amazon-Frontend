e1f83c56fbd1c8219b24dc5ce181b65ca58aae77
KoryunWeConstruct <koryun986@gmail.com>
2024-03-25 14:48:54 +0400

    fix(product page image)

--- src/app/products/[id]/_components/ProductImageGroup.tsx
+++ src/app/products/[id]/_components/ProductImageGroup.tsx
@@ -15,10 +15,10 @@ const ProductImageGroup: FC<ProductImageGroupProps> = ({main_image, images}) => 
   return (
     <PreviewGroup>
       <div className="col-start-1 col-span-2 row-start-1 row-span-2">
-        <Image width={"100%"} height={"100%"} src={ApiConstants.PUBLIC_ASSETS_URL+main_image} alt={"Product Image"} />
+        <Image width={"100%"} height={"100%"} src={ApiConstants.PUBLIC_ASSETS_URL+main_image} alt={"Product Image"} className={"object-contain"} />
       </div>
       {!!images.length && images.map((image, index) => (
-        <div className="h-full row-span-1" key={index}><Image width={"100%"} height={"100%"} className="mx-auto" src={ApiConstants.PUBLIC_ASSETS_URL+image} /></div>
+        <div className="h-full row-span-1" key={index}><Image width={"100%"} height={"100%"} className="mx-auto object-contain" src={ApiConstants.PUBLIC_ASSETS_URL+image} /></div>
       ))}
     </PreviewGroup>
   )
